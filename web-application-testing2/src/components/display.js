eb13424cd8a46a54146eea981734bef06595d088
Connor Claxton <czclaxton@gmail.com>
2019-08-08 18:19:32 -0500

    attempted testing but finished MVP

--- web-application-testing2/src/components/display.js
+++ web-application-testing2/src/components/display.js
@@ -1,12 +1,6 @@
 import React from "react";
 
 const Display = props => {
-  // if (props.strikes >= 3) {
-  //   return setStrikes(0);
-  // } else {
-  //   props.strikes = props.strikes;
-  // }
-
   return (
     <div>
       <div>Strikes: {props.strikes}</div>
